a663acd2487e136e95339cbbe942ad7a1c1f7709
Ruchiket100 <RuchiketBorse1@gmail.com>
2023-06-25 10:46:14 +0530

    initial typescript setup

---
+++ build/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("Hello World");
+let a = 'Ruchiket';
+let b = 12;
+function print(name) {
+    console.log(name);
+}
+function add(a, b) {
+    return a + b;
+}
+print("Ruchiket100");
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+console.log("Hello World")
+
+let a : string = 'Ruchiket';
+let b : number = 12;
+
+function print(name:string){
+    console.log(name)
+}
+
+function add(a: number, b: number){
+    return a + b;
+}
+print("Ruchiket100")
