3053c6b0497a3bcfe924239f4a663276ccd56384
Ruchiket100 <RuchiketBorse1@gmail.com>
2023-06-30 19:23:25 +0530

    Utility Types

--- build/js/main.js
+++ build/js/main.js
@@ -1,22 +1,39 @@
 "use strict";
-// interface Cart {
-// apple: number
-// banana: number
-// grapes: number
-// }
-const FoodCart = {
-    apple: 10,
-    banana: 20,
-    grapes: 30
+// Utility Types
+// it will construct type set of properties of Marks set to optional 
+const updateMarks = (marks, propsToUpdate) => {
+    return Object.assign(Object.assign({}, marks), propsToUpdate);
 };
-let prop = 'apple';
-const shinchan = {
-    name: 'Shinchan',
-    id: 1,
-    classes: ['English', 'Drawing']
+let StudentMarks = {
+    studentId: 1,
+    subject: 'math',
+    grade: 100
 };
-let myIncome = {
-    salary: 100,
-    investment: 200,
-    sideHustle: 1000
+console.log(updateMarks(StudentMarks, { grade: 50 }));
+// Required
+// all properties of type Marks are required
+const changeStudent = (assign) => {
+    return assign;
 };
+const cars = {
+    Virtus: { power: 120, milage: 20 },
+    i20: { power: 100, milage: 25 }
+};
+console.log(cars.i20);
+const stu1Grade = {
+    studentId: 1,
+    grade: 100
+};
+const stuSubject = {
+    subject: 'math'
+};
+const practicalCar = "i20";
+const assignBook = (name, id) => {
+    return { name, id };
+};
+// if we change the type of parameters, it cannot be reflect in Book
+// to make it reflect we use Return Type
+const assignNewBook = (name, id) => {
+    return { name, id };
+};
+const bookName = assignNewBook('bookName', 1);
--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,94 @@
-// Index Signature
-interface Cart {
-    readonly [index: string]: number
+// Utility Types
 
+// Partial
+
+interface Marks {
+    studentId: number
+    subject: string
+    grade: number
 }
 
-// interface Cart {
-// apple: number
-// banana: number
-// grapes: number
-// }
 
-const FoodCart: Cart = {
-    apple: 10,
-    banana: 20,
-    grapes: 30
+// it will construct type set of properties of Marks set to optional 
+const updateMarks = (marks: Marks, propsToUpdate: Partial<Marks>): Marks => {
+    return { ...marks, ...propsToUpdate }
 }
 
-let prop: string = 'apple'
-// console.log(FoodCart[prop])
-//Element implicitly has an 'any' type because expression of type 'any' can't be used to index type 'Cart'.
-
-// ---------------------------------------
-
-interface Student {
-    name: string
-    id: number
-    classes?: string[]
+let StudentMarks: Marks = {
+    studentId: 1,
+    subject: 'math',
+    grade: 100
 }
 
-const shinchan: Student = {
-    name: 'Shinchan',
-    id: 1,
-    classes: ['English', 'Drawing']
+console.log(updateMarks(StudentMarks, { grade: 50 }))
+
+
+// Required
+// all properties of type Marks are required
+const changeStudent = (assign: Required<Marks>): Marks => {
+    return assign
 }
 
-// for (let key in shinchan)
-//     console.log(shinchan[key as keyof Student])
-
-// ---------------------------------------
-
-// interface income {
-//     [key: string]: number
-// }
-
-type Streams = 'salary' | 'investment' | 'sideHustle'
-
-type income = Record<Streams, number>
-
-let myIncome: income = {
-    salary: 100,
-    investment: 200,
-    sideHustle: 1000
+// Record - it construct an object with properties of Keys and values of Type
+type Car = "Virtus" | "i20"
+type CarSpetiality = {
+    power: number
+    milage: number
 }
+
+const cars: Record<Car, CarSpetiality> = {
+    Virtus: { power: 120, milage: 20 },
+    i20: { power: 100, milage: 25 }
+}
+
+console.log(cars.i20)
+
+
+// Pick and Omit
+
+type studentGrade = Pick<Marks, "studentId" | "grade">
+
+const stu1Grade: studentGrade = {
+    studentId: 1,
+    grade: 100
+}
+
+type studentSubject = Omit<Marks, 'studentId' | 'grade'>
+
+const stuSubject: studentSubject = {
+    subject: 'math'
+}
+
+// Extract and Exclude
+
+type moreSafer = Extract<Car, 'Virtus'>
+
+type morePractical = Exclude<Car, 'Virtus'>
+
+const practicalCar: morePractical = "i20"
+
+// Nonnullable
+
+type Names = "Jay" | 'Jack' | null | undefined
+
+type PossibleNames = NonNullable<Names>
+
+// ReturnType
+
+type Book = { name: string, id: number }
+
+const assignBook = (name: string, id: number): Book => {
+    return { name, id }
+}
+
+// if we change the type of parameters, it cannot be reflect in Book
+// to make it reflect we use Return Type
+
+const assignNewBook = (name: string, id: number) => {
+    return { name, id }
+}
+
+type NewAssign = ReturnType<typeof assignNewBook>
+
+const bookName: NewAssign = assignNewBook('bookName', 1)
+
